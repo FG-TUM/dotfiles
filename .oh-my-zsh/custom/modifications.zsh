3e302d8d47d62bc3c8a55bf0a5012a7b6f767287
FG-TUM <f.gratl@tum.de>
2018-12-24 14:12:39 +0100

    ldot for now takes path

--- .oh-my-zsh/custom/modifications.zsh
+++ .oh-my-zsh/custom/modifications.zsh
@@ -4,6 +4,18 @@ alias rm='rm -I'
 alias ls='ls --color=always'
 alias dmesg='dmesg --color=always --human'
 
+# lists all hidden files
+unalias ldot
+ldot()
+{
+    # locally enables supressing glob fail output
+    setopt local_options no_nomatch
+
+    searchPath=${1}
+    [[ "${searchPath}" != "" && "${searchPath}" != */ ]] && searchPath=${searchPath}/
+    ls -ld ${searchPath}.* 2>/dev/null
+}
+alias lsdot=ldot
 
 # fixes for vi style
 #bindkey -v "k" up-line-or-beginning-search
