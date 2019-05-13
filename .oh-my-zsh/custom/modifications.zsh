a9ae78c37a63851d5f348b059898cd5d2820eca8
FG-TUM <f.gratl@tum.de>
2019-05-13 13:31:38 +0200

    alias for cleaning svn repos

--- .oh-my-zsh/custom/modifications.zsh
+++ .oh-my-zsh/custom/modifications.zsh
@@ -4,6 +4,8 @@ alias rm='rm -I'
 alias ls='ls --color=always'
 alias dmesg='dmesg --color=always --human'
 
+# removes all locally missing files
+alias svnCleanup="svn status  | sed --quiet -e 's/^\! *\(.*\)/\1/p' | xargs svn rm"
 
 # fixes for vi style
 #bindkey -v "k" up-line-or-beginning-search
